3f7f2c7aea9973dc4bf4df0404114b3a63824282
srilathasagili-crypto <srilathasagili@gmail.com>
2026-05-04 16:34:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,18 @@
 import streamlit as st
+import pickle
+import numpy as np
 
-st.title("Test app")
-st.write("Working")
+load model
+model = pickle.load(open("diamond_pipeline.pkl", "rb"))
+
+st.title("Diamond Price Prediction 💎")
+
+inputs
+carat = st.number_input("Carat")
+depth = st.number_input("Depth")
+
+prediction
+if st.button("Predict"):
+    input_data = np.array([[carat, depth]])
+    prediction = model.predict(input_data)
+    st.success(f"Predicted Price: {prediction[0]}")
